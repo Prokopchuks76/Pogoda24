a9de2781e13c4feddff668410ccf00f380a8dc7d
Oleksandr <prockopchuks76@gmail.com>
2021-07-02 19:53:33 +0300

    init commit

---
+++ src/main/java/com/wether/Pogoda24Application.java
@@ -0,0 +1,13 @@
+package com.wether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pogoda24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pogoda24Application.class, args);
+	}
+
+}
